966bf56cde48f19d30970cb8c0c60cdf0ef15a2e
paxmix <manh1998nb@gmail.com>
2026-07-12 11:28:26 +0700

    remove . .. in zsh

--- home/config/zsh.nix
+++ home/config/zsh.nix
@@ -352,7 +352,6 @@
 
       # cmp opts
       zstyle ':completion:*' menu select # tab opens cmp menu
-      zstyle ':completion:*' special-dirs true # force . and .. to show in cmp menu
       zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS} ma=0\;33 # colorize cmp menu
       zstyle ':completion:*' squeeze-slashes false # explicit disable to allow /*/ expansion
     '';
