7de9e52953ad052d3fede77aa514d7fa79549e66
paxmix <manh1998nb@gmail.com>
2026-07-14 00:12:53 +0700

    add gtk4 force dark theme

--- home/config/gtk.nix
+++ home/config/gtk.nix
@@ -17,6 +17,9 @@
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
     };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
   };
 
   # 2. Dconf settings for Dark Mode and Terminal
