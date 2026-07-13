15527df70598972eb1d500b70fa802aea6175626
paxmix <manh1998nb@gmail.com>
2026-07-13 10:10:42 +0700

    add trouble and autocmds

--- home/config/nixvim/default.nix
+++ home/config/nixvim/default.nix
@@ -14,6 +14,7 @@
       ./plugins/lsp.nix
       ./plugins/mini.nix
       ./plugins/snacks.nix
+      ./plugins/trouble.nix
     ];
 
     colorschemes.tokyonight = {
@@ -25,6 +26,36 @@
       mapleader = " ";
       maplocalleader = " ";
     };
+
+    autoCmd = [
+      {
+        event = [ "TextYankPost" ];
+        callback = {
+          __raw = ''
+            function()
+              vim.highlight.on_yank({
+                higroup = "IncSearch", 
+                timeout = 150,        
+              })
+            end
+          '';
+        };
+      }
+      {
+        event = [ "BufReadPost" ];
+        callback = {
+          __raw = ''
+            function()
+              local mark = vim.api.nvim_buf_get_mark(0, '"')
+              local lcount = vim.api.nvim_buf_line_count(0)
+              if mark[1] > 0 and mark[1] <= lcount then
+                pcall(vim.api.nvim_win_set_cursor, 0, mark)
+              end
+            end
+          '';
+        };
+      }
+    ];
 
     plugins = {
       luasnip.enable = true;
@@ -53,7 +84,6 @@
     extraPackages = with pkgs; [
       ripgrep
       fd
-      nixfmt
     ];
   };
 }
