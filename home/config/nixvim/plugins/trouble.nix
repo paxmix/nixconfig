9e53416feae31b3d216ce06a7fd950fc079db44c
paxmix <manh1998nb@gmail.com>
2026-07-14 09:43:00 +0700

    add nixvim diagnostics icons

--- home/config/nixvim/plugins/trouble.nix
+++ home/config/nixvim/plugins/trouble.nix
@@ -4,6 +4,19 @@ _: {
     underline = true;
     update_in_insert = false;
     severity_sort = true;
+    signs = {
+      text = {
+        # Level = "Icon"
+        "__raw" = ''
+          {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN]  = "󰀪 ",
+            [vim.diagnostic.severity.INFO]  = "󰋽 ",
+            [vim.diagnostic.severity.HINT]  = "󰌶 ",
+          }
+        '';
+      };
+    };
   };
 
   highlight = {
