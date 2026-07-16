69a04fd6d61eaf5aebeeb186ae63bbd3ef72a71f
paxmix <manh1998nb@gmail.com>
2026-07-16 19:02:34 +0700

    configure hydro

--- home/config/shell.nix
+++ home/config/shell.nix
@@ -18,6 +18,14 @@
 
     interactiveShellInit = ''
       set -g fish_greeting # Clear the welcome message
+
+      set -g hydro_color_pwd "7aa2f7"
+      set -g hydro_color_git "bb9af7"
+      set -g hydro_color_error "f7768e"
+      set -g hydro_color_prompt "9ece6a"
+      set -g hydro_color_duration "e0af68"
+      set -g hydro_symbol_prompt "󰘧"
+      set -g hydro_cmd_duration_threshold 5000
     '';
   };
 }
