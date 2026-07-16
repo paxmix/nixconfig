04e307866a583885c01c325b9c4b4059463d110b
paxmix <manh1998nb@gmail.com>
2026-07-16 18:56:18 +0700

    add hydro plugin

--- home/config/shell.nix
+++ home/config/shell.nix
@@ -1,4 +1,4 @@
-_: {
+{ pkgs, ... }: {
   programs.fish = {
     enable = true;
 
@@ -8,5 +8,16 @@ _: {
       ll = "eza -la --icons=auto --color=always";
       lt = "eza -Ta --icons=auto --color=always";
     };
+
+    plugins = [
+      {
+        name = "hydro";
+        src = pkgs.fishPlugins.hydro.src;
+      }
+    ];
+
+    interactiveShellInit = ''
+      set -g fish_greeting # Clear the welcome message
+    '';
   };
 }
