e66fdcead4d1f12a73969084d19fdfb03f4dedb9
paxmix <manh1998nb@gmail.com>
2026-07-26 14:40:08 +0700

    remove hydro plugin

--- home/config/shell.nix
+++ home/config/shell.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+_: {
   programs.fish = {
     enable = true;
 
@@ -9,23 +9,8 @@
       lt = "eza -Ta --icons=auto --color=always";
     };
 
-    plugins = [
-      {
-        name = "hydro";
-        src = pkgs.fishPlugins.hydro.src;
-      }
-    ];
-
     interactiveShellInit = ''
       set -g fish_greeting # Clear the welcome message
-
-      set -g hydro_color_pwd "7aa2f7"
-      set -g hydro_color_git "bb9af7"
-      set -g hydro_color_error "f7768e"
-      set -g hydro_color_prompt "9ece6a"
-      set -g hydro_color_duration "e0af68"
-      set -g hydro_symbol_prompt "󰘧"
-      set -g hydro_cmd_duration_threshold 5000
     '';
   };
 }
