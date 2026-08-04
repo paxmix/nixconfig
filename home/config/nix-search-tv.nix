84842ef62b99a2992b8dbd9b6ffb8d8b2823e55d
paxmix <manh1998nb@gmail.com>
2026-08-04 15:20:17 +0700

    enable fzf fish integration

--- home/config/nix-search-tv.nix
+++ home/config/nix-search-tv.nix
@@ -6,7 +6,26 @@ _: {
     };
     television = {
       enable = true;
+    };
+    fzf = {
+      enable = true;
       enableFishIntegration = true;
+      changeDirWidgetOptions = [
+        "--walker-skip .git,node_modules,target"
+        "--preview 'tree -C {}'"
+      ];
+      historyWidgetOptions = [
+        "--with-nth 1,3.."
+        "--bind 'alt-t:change-with-nth(2..|3..|1,3..)'"
+        "--bind 'ctrl-y:execute-silent(echo -n {3..} | wl-copy)+abort'"
+        "--color header:italic"
+        "--header 'Press CTRL-Y to copy command into clipboard'"
+      ];
+      fileWidgetOptions = [
+        "--walker-skip .git,node_modules,target"
+        "--preview 'bat -n --color=always {}'"
+        "--bind 'ctrl-/:change-preview-window(down|hidden|)'"
+      ];
     };
   };
 }
